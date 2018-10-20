0756880748c1c13363b244351bcefa5d2acfcc62
vunnysher <vunnysher@gmail.com>
2018-10-20 15:10:41 +0200

    Added arrows deactivation

--- public/js/script.js
+++ public/js/script.js
@@ -3,6 +3,9 @@ var daysData = {}
 var currentMonthDisplaying = 7;
 var currentYearDisplaying = 2018;
 
+var leftArrowDeactivated = false;
+var rightArrowDeactivated = false;
+
 function slideToTheRight() {
     if (!shouldIWait) {
         var nextPageContent = document.getElementsByClassName("right")[0];
@@ -25,6 +28,12 @@ function slideToTheRight() {
         if(daysData[currentYearDisplaying]==null || daysData[currentYearDisplaying][currentMonthDisplaying].length == 1) // that means there is no info about trade-off days yet
             spawnNoDaysAvaibleInfo();
         else spawnDays();
+
+        if(leftArrowDeactivated) { //enable the left arrow
+            var leftArrow = document.getElementById("arrowLeft");
+            leftArrow.className = "arrow";
+            leftArrow.style.pointerEvents = 'auto'; 
+        }
     }
 }
 
@@ -50,6 +59,12 @@ function slideToTheLeft() {
         if(daysData[currentYearDisplaying]==null || daysData[currentYearDisplaying][currentMonthDisplaying].length == 1) // that means there is no info about trade-off days yet
             spawnTheLawStartInfo();
         else spawnDays();
+
+        if(rightArrowDeactivated) { //enable the left arrow
+            var rightArrow = document.getElementById("arrowRight");
+            rightArrow.className = "arrow";
+            rightArrow.style.pointerEvents = 'auto'; 
+        }
     }
     
 }
@@ -76,6 +91,12 @@ function spawnNoDaysAvaibleInfo() {
     daysContainer.innerHTML = "";
     daysContainer.innerHTML += "<h3>Jeszcze nie ma informacji o niedzielach bez handlu na ten miesiąc.</br></h3>"
     daysContainer.innerHTML += "<h4>Zostaną one dodane wraz z pojawieniem się dat.</br></h4>"
+
+    //disable the right arrow
+    var rightArrow = document.getElementById("arrowRight");
+    rightArrow.className = "arrow arrowDeactivated";
+    rightArrow.style.pointerEvents = 'none'; 
+    rightArrowDeactivated = true;
 }
 function spawnTheLawStartInfo() {
     var currentPageContentContainer = document.getElementsByClassName("current")[0];
@@ -85,6 +106,12 @@ function spawnTheLawStartInfo() {
     daysContainer.innerHTML = "";
     daysContainer.innerHTML += "<h3>W tym czasie ustawa <a href='https://orka.sejm.gov.pl/proc8.nsf/ustawy/870_u.htm'>o ograniczeniu handlu w niedziele i święta oraz w niektóre inne dni</a> nie była jeszcze wdrożona.</br></h3>"
     daysContainer.innerHTML += "<h4>Zaczęła ona działać z dniem <b>10 Stycznia 2018 r.</b> i obowiązuje aż do teraz.</br></h4>"
+
+    //disable the left arrow
+    var leftArrow = document.getElementById("arrowLeft");
+    leftArrow.className = "arrow arrowDeactivated";
+    leftArrow.style.pointerEvents = 'none'; 
+    leftArrowDeactivated = true;
 }
 
 function getServerData() {
