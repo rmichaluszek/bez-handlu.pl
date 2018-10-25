479d15c4495fd8fcec7481ed4748ba6528995ed1
vunnysher <vunnysher@gmail.com>
2018-10-25 19:59:04 +0200

    Renaming application to Bez-handlu.pl

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const mongoose = require('mongoose');
 const app = express()
 const port = 8080
 
+
 app.get('/api/get-data', (req, res) => {
-    var testData = {
+   /* var testData = {
         '2018' : [
             [[1,true],[8,false],[15,false],[22,true],[29,true]],
             [[1,true],[8,false],[15,false],[22,true]],
@@ -18,10 +20,17 @@ app.get('/api/get-data', (req, res) => {
             [[1,true],[8,false],[15,false],[22,true],[29,true]],
             [[1,true],[8,false],[15,false],[22,true]]
         ]
-    }
+    }*/
+
+    
+
     res.send(testData);
 });
 
 app.use(express.static('public'))
 
-app.listen(port, () => console.log('Niedziele-bez-handlu server is running on port 8080...'));
+mongoose.connect("mongodb://localhost:27017/bez-handlu-db",{ useNewUrlParser: true }).then(
+    () => {console.log('Connected to the database') },
+    err => { console.log('Error connecting to the database: ' +err)
+});
+app.listen(port, () => console.log('Bez-handlu.pl server is running on port 8080...'));
