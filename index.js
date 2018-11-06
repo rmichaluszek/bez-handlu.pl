5e1147558f94499c71210bfa7f677f8a1fc689e8
Rafał Michałuszek <vunnysher@gmail.com>
2018-11-06 21:44:18 +0000

    Added connecting to the mongo database

--- index.js
+++ index.js
@@ -20,17 +20,32 @@ app.get('/api/get-data', (req, res) => {
             [[1,"sunday"],[8,"holiday"],[15,"holiday"],[22,"sunday"],[29,"sunday"]],
             [[1,"sunday"],[8,"holiday"],[15,"holiday"],[22,"sunday"]]
         ]
-    }
-
-    
-
+    }  
     res.send(testData);
 });
 
+var getDataSchema = mongoose.Schema({
+    year: Number,
+    months: Array,
+    _id: { // prever app from getting id of the year because that's not needed
+      type: String,
+      select: false,
+      required: true,
+    }
+});
+var Data = mongoose.model('data', getDataSchema,'days');
+
 app.use(express.static('public'))
 
-mongoose.connect("mongodb://localhost:27017/bez-handlu-db",{ useNewUrlParser: true }).then(
+mongoose.connect("mongodb://localhost:27017/bez-handlu",{ useNewUrlParser: true }).then(
     () => {console.log('Connected to the database') },
-    err => { console.log('Error connecting to the database: ' +err)
-});
+    err => { console.log('Error connecting to the database: ' +err)});
+
+    console.log("super");  
+            
+    Data.find(function(err, res){
+        console.log(res);
+    });
+
 app.listen(port, () => console.log('Bez-handlu.pl server is running on port 8080...'));
+
